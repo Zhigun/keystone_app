cca1e16b4be51595b7c05e4de28a13feb559b09d
Ivan <zhigun.ivan@mail.ru>
2015-02-11 17:49:10 +0200

    "config/production , development"

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,4 @@
-KeystoneApp::Application.application.configure do
+KeystoneApp::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
