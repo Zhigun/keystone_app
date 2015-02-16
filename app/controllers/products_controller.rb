6064377b3e1bff02a729e5aa7c1e0da428dd7bed
ivan <ivan@elephantheadsoft.com>
2015-02-16 15:42:00 +0200

    "Final CSS chainges"

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -32,7 +32,8 @@ class ProductsController < ApplicationController
 
     respond_to do |format|
       if @product.save
-        format.html { redirect_to @product, notice: 'Product was successfully created.' }
+        flash[:success] = 'Product was successfully created.'
+        format.html { redirect_to @product }
         format.json { render :show, status: :created, location: @product }
       else
         format.html { render :new }
@@ -46,7 +47,8 @@ class ProductsController < ApplicationController
   def update
     respond_to do |format|
       if @product.update(product_params)
-        format.html { redirect_to @product, notice: 'Product was successfully updated.' }
+        flash[:success] = 'Product was successfully updated.'
+        format.html { redirect_to @product }
         format.json { render :show, status: :ok, location: @product }
       else
         format.html { render :edit }
