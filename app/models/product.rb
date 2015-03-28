b1f6395448387adab46f5022bbe1f071539bd830
Sergey <deadman144@gmail.com>
2015-03-28 17:25:26 +0200

    "Disabled left menu if faild login"

--- app/models/product.rb
+++ app/models/product.rb
@@ -8,4 +8,5 @@ class Product < ActiveRecord::Base
   default_scope -> { order('created_at DESC') }
   validates :category_id, presence: true
   validates :img, presence: true
+  validate :title, allow_blank: false
 end
