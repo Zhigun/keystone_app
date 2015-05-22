de1dcc0e983ae761f93dd598f0843fc90b12d216
ivan <ivan@elephantheadsoft.com>
2015-05-22 12:18:29 +0300

    Test commit 3 factor auth 1111

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   #test comment
-  #test comment
+  #test comment1
   protect_from_forgery with: :exception
   include SessionsHelper
 end
